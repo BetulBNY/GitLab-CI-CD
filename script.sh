7dd0ae58b8563b34a60eefc7d204ba374c1194dd
Betül Nur Yıldırım <betulbny@gmail.com>
2026-01-21 07:09:52 +0000

    Starting a server / background process within a job.

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Check if OPENAI_API_KEY is set and not empty
-if [ -z "$OPENAI_API_KEY" ]; then
-    echo "Error: OPENAI_API_KEY is not set or is empty."
-    exit 1
-else
-    echo "Success: OPENAI_API_KEY is set."
-fi
